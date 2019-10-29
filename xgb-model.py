335dd0cf3eb903808463ba03656853fd8f7bfe3b
Mark Schmidt <mschmidt@bizstream.com>
2019-10-29 06:17:25 -0400

    got a basic XGBoost model working

--- xgb-model.py
+++ xgb-model.py
@@ -6,8 +6,9 @@ import pathlib
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import make_scorer, accuracy_score
+import numpy as np
+import xgboost as xgb
+from sklearn.metrics import mean_squared_error
 import utils.pymark as pymark
 
 # https://colab.research.google.com/drive/1md0xrsTWjrYH3R8l5z96FpWiy8IuWZWT#scrollTo=uFt2-9epOpN4
@@ -33,21 +34,25 @@ train_dataset, train_labels, test_dataset, test_labels, output_column_name = pym
 print(train_dataset.tail())
 input_size = len(train_dataset.keys())
 
-# LinearRegression Model (from sklearn) (Create and train/fit)
-model = LinearRegression()
+# https://www.datacamp.com/community/tutorials/xgboost-in-python
+# messing with the hyper parameters really makes a difference
+model = xgb.XGBRegressor(objective ='reg:linear', colsample_bytree = 0.3, learning_rate = 0.1,
+                max_depth = 5, alpha = 10, n_estimators = 1000)
+
 model.fit(train_dataset, train_labels)
 
 def test_model(model, label, X, y, output_column_name):
-  score = model.score(X, y)
 
   count = y.shape[0]
 
   y_predictions = model.predict(X).flatten()
   pymark.compare_actual_and_predicted(y, y_predictions)
 
+  rmse = np.sqrt(mean_squared_error(y, y_predictions))
+
   label = f'{label} ({count} records)'
 
-  print(f'{label} Score: {score:5.2f} {output_column_name}')
+  print(f'{label} RMSE: {rmse:5.2f} {output_column_name}')
 
   pymark.plot_actual_and_predicted(label, y, y_predictions, output_column_name)
 
